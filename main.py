1305d983e1e8bee8380a3a4e82fa1a1d78f2723e
UiUian <60983308+Vivian-zzz@users.noreply.github.com>
2023-09-02 22:54:05 -0400

    Update

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+def f(s):
+    di={}
+    largest=0
+    left=0
+    for i in range(len(s)):
+        check = di[s[i]]+1 if s[i] in di else 0
+        left = max(left, check)
+        largest=max(largest,i-left+1)
+        di[s[i]]=i ## i + 1
+    return largest
+
+if __name__ == "__main__":
+    print(f("cabcdcc"))
